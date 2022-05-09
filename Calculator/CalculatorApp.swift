b55d909f0295831d6ddd205bbeb6f8c0843d4a4c
Liam Doran <liambdoran@gmail.com>
2022-05-08 21:58:58 -0700

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Liam Doran on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
